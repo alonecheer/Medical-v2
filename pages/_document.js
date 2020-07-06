5c030a56b214d835bc28a9a249013c0f1f230e8d
alonecheer <alonecheer@hotmail.com>
2020-07-06 23:33:06 +0700

    antd

--- pages/_document.js
+++ pages/_document.js
@@ -1,49 +1,26 @@
-import Document, { Html, Main, NextScript ,Head} from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
-
+import Document, { Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
-        })
-
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      }
-    } finally {
-      sheet.seal()
-    }
-  }
-  render(){
-  return(
-    <Html>
-      <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-      <link rel="icon" href="/static/images/psu_icon.png"></link>
-      
-      <link rel='stylesheet'   href='https://cdnjs.cloudflare.com/ajax/libs/antd/3.22.0/antd.min.css' />
-      
-      </Head>
-      <body>
-        
-        <Main/>
-        <NextScript/>
-      </body>
-    </Html>
-  )
-}
-}
-
+    static getInitialProps({ renderPage }) {
+      const sheet = new ServerStyleSheet();
+      const page = renderPage((App) => (props) =>
+sheet.collectStyles(<App {...props} />),
+);
+      const styleTags = sheet.getStyleElement();
+      return { ...page, styleTags };
+     }
+render() {
+     return (
+         <html>
+          <Head >
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/antd/3.22.0/antd.min.css' />
+          {this.props.styleTags}
+          </Head>
+          <body>
+           <Main /> 
+           <NextScript />
+          </body>
+        </html>
+)}}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,122 @@
-import { Table, Tag, Space } from 'antd';
-const App = () => {
+import React, { useState, useEffect } from "react";
+import { Table, Tag, Space, Input  } from 'antd';
+import styled from "styled-components";
+const StyledWrapper = styled.div`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  /* ------------------------------------ Start header  ------------------------------------*/
+  .container {
+    max-width: 960px;
+    width: 100%;
+    margin: 0 auto;
+  }
+  header {
+    width: 100%;
+    height: 120px;
+    background-color: #01273c;
+    border-top: 5px solid #40aed7;
+  }
+  .nav-grid {
+    display: grid;
+    grid-template-columns: 225px 1fr;
+  }
+  .logo {
+    color: white;
+    margin-top: 20px;
+  }
+  h1 {
+    color: white;
+  }
+  .logo h1 {
+    font-size: 28px;
+    text-transform: uppercase;
+  }
+  .logo span {
+    font-size: 12px;
+  }
+  ul.menu {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    justify-items: center;
+    margin-top: 7%;
+  }
+  ul.menu li {
+    text-decoration: none;
+    list-style: none;
+  }
+  ul.menu li a {
+    text-decoration: none;
+    color: white;
+    text-transform: uppercase;
+    transition: 0.3 ease-ease-in-out;
+    padding: 5px 10px;
+  }
+  ul.menu li a:hover {
+    background: #40aed7;
+    padding: 5px 10px;
+    border-radius: 5px;
+  }
+  /* ------------------------------------ End header  ------------------------------------*/
+
+  /* ------------------------------------ Start section  ------------------------------------*/
+
+  .content {
+    margin: 0 auto;
+    max-width: 960px;
+    height: auto;
+  }
+  .content-heading {
+  }
+  /* ------------------------------------ End section  ------------------------------------*/
+  /* ------------------------------------ Start Footer  ------------------------------------*/
+
+  .copyright {
+    width: 100%;
+    height: 60px;
+    background-color: #01273c;
+  }
+  .copyright-grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    height: 60px;
+    color: white;
+  }
+  .copytext {
+    justify-self: flex-start;
+    align-self: center;
+  }
+  .creadit {
+    justify-self: flex-end;
+    align-self: center;
+  }
+  .content-heading img {
+    width: 100%;
+  }
+  .nav li ul {
+      position: absolute;
+      display: none;
+      width: inherit;
+    }
+  /* ------------------------------------ End Footer  ------------------------------------*/
+  Input { 
+      margin-top: 15%;
+      margin-left: 35%;
+      margin-bottom: 15%;
+      width: 300px;
+      padding-left: 10px;
+  }
+`;
+
+const Index = () => {
+    const [text,setText] = useState('')
+    console.log(text)
+    const checktext = (e) => {
+        setText(e)
+        
+    }
     const columns = [
         {
           title: 'Name',
@@ -72,9 +189,78 @@ const App = () => {
           tags: ['cool', 'teacher'],
         },
       ];
-    return (
-        <div>
-            <Table columns={columns} dataSource={data} />My App</div>
-    )
-}
-export default App
+      const onChange = (pagination, filters, sorter, extra) =>{
+        console.log('params', pagination, filters, sorter, extra);
+      }
+  return (
+    <StyledWrapper>
+      <title>Homepage</title>
+      {/*------------------------------------ Start Header ------------------------------------*/}
+      <header>
+        <nav>
+          <div className="container">
+            <div className="nav-grid">
+              <div className="logo">
+                <h1>Psu</h1>
+                <span>Procurement system</span>
+              </div>
+              <div className="fr">
+                <ul className="menu">
+                  <li>
+                    <a href="#">Add cart1</a>
+                  </li>
+                  <li>
+                    <a href="#">Add cart1</a>
+                  </li>
+                  <li>
+                    <a href="#">profile</a>
+                    <ul>
+                      <li>
+                        <a href="#">ข้อมูลส่วนตัว</a>
+                      </li>
+                      <li>
+                        <a href="#">ออกจากระบบ</a>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </header>
+      {/*------------------------------------ End Header ------------------------------------*/}
+
+      {/*------------------------------------ Start Section ------------------------------------*/}
+      <section className="content">
+        <div className="container">
+          <div className="content-heading">
+            {/* <img src="/static/images/bg.jpg" /> */}
+          </div>
+          <Input placeholder="Basic usage" onChange={ (e)=>{checktext(e.target.value)}}/>
+          <Table columns={columns} dataSource={data} onChange={onChange} />
+        </div>
+      </section>
+      {/*------------------------------------ End Section ------------------------------------*/}
+
+      {/*------------------------------------ Start Footer ------------------------------------*/}
+      <footer>
+        <div className="copyright">
+          <div className="container">
+            <div className="copyright-grid">
+              <div className="copytext">
+                <p>Copyright © 2020 Jetnipat - All Rights Reserved</p>
+              </div>
+              <div className="creadit">
+                <p>ccnp</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      {/*------------------------------------ End Footer ------------------------------------*/}
+      
+    </StyledWrapper>
+  );
+};
+export default Index;
