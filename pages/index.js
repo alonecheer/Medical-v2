f5c5f2424fca904da624f8f4446669764c4ddc32
alonecheer <alonecheer@hotmail.com>
2020-07-08 17:17:32 +0700

    responsive

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { MDBDataTable } from 'mdbreact';
-import data from './data_json'
+import React from "react";
+import { MDBDataTable } from "mdbreact";
+import data from "./data_json";
 import styled from "styled-components";
 const StyledWrapper = styled.div`
-* {
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
@@ -71,6 +71,9 @@ const StyledWrapper = styled.div`
   }
   .content-heading {
   }
+  .content-heading2 {
+    display: none;
+  }
   /* ------------------------------------ End section  ------------------------------------*/
   /* ------------------------------------ Start Footer  ------------------------------------*/
 
@@ -97,18 +100,85 @@ const StyledWrapper = styled.div`
     width: 100%;
   }
   .nav li ul {
-      position: absolute;
-      display: none;
-      width: inherit;
-    }
+    position: absolute;
+    display: none;
+    width: inherit;
+  }
   /* ------------------------------------ End Footer  ------------------------------------*/
+  @media screen and (max-width: 414px) {
+    header {
+      width: 100%;
+      height: 120px;
+      background-color: #01273c;
+      border-top: 5px solid #40aed7;
+      margin: 0 auto;
+    }
+    .copyright {
+      width: 100%;
+      height: 80px;
+      background-color: #01273c;
+    }
+    .copyright-grid {
+      display: grid;
+      grid-template-columns: 1fr;
+      height: 60px;
+      color: white;
+    }
+    ul.menu {
+      display: grid;
+      grid-template-columns: repeat(1, 1fr);
+      justify-items: center;
+      margin-top: 7%;
+    }
+    ul.menu li {
+      text-decoration: none;
+      list-style: none;
+      margin-top: 15px;
+    }
+    ul.menu li a {
+      text-decoration: none;
+      color: white;
+      text-transform: uppercase;
+      transition: 0.3 ease-ease-in-out;
+      padding: 5px 10px;
+    }
+    ul.menu li a:hover {
+      background: #40aed7;
+      padding: 5px 10px;
+      border-radius: 5px;
+    }
+    .nav-grid {
+      display: grid;
+      grid-template-columns: 190px 1fr;
+    }
+    .logo {
+      color: white;
+      margin-top: 20px;
+    }
+    h1 {
+      color: white;
+    }
+    .logo h1 {
+      font-size: 28px;
+      text-transform: uppercase;
+    }
+    .logo span {
+      font-size: 12px;
+    }
+    .content-heading {
+      display: none;
+    }
+    .not_res {
+      color: red;
+      text-align: center;
+    }
+  }
 `;
 
 const DatatablePage = () => {
-
   return (
     <StyledWrapper>
-<title>Homepage</title>
+      <title>Homepage</title>
       {/*------------------------------------ Start Header ------------------------------------*/}
       <header>
         <nav>
@@ -116,8 +186,7 @@ const DatatablePage = () => {
             <div className="nav-grid">
               <div className="logo">
                 <h1>Dictionary</h1>
-                <span>
-Medical terminology</span>
+                <span>Medical terminology</span>
               </div>
               <div className="fr">
                 <ul className="menu">
@@ -139,12 +208,10 @@ Medical terminology</span>
       <section className="content">
         <div className="container">
           <div className="content-heading">
-          <MDBDataTable
-      striped
-      bordered
-      small
-      data={data}
-    />
+            <MDBDataTable striped bordered small data={data} />
+          </div>
+          <div className="not_res">
+            The website does not support this device.
           </div>
         </div>
       </section>
@@ -156,10 +223,10 @@ Medical terminology</span>
           <div className="container">
             <div className="copyright-grid">
               <div className="copytext">
-                <p>Copyright © 2020 Jetnipat - All Rights Reserved</p>
+                <p>Copyright © 2020 Chatchaya - All Rights Reserved</p>
               </div>
               <div className="creadit">
-                <p>ccnp</p>
+                <p>Meen</p>
               </div>
             </div>
           </div>
@@ -168,6 +235,6 @@ Medical terminology</span>
       {/*------------------------------------ End Footer ------------------------------------*/}
     </StyledWrapper>
   );
-}
+};
 
 export default DatatablePage;
