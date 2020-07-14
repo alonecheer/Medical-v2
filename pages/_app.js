41a0b777a892b8e3fcaeada53d3e5a5f9ecb43ae
alonecheer <alonecheer@hotmail.com>
2020-07-15 05:40:09 +0700

    deleate  fonts

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import 'bootstrap-css-only/css/bootstrap.min.css'; 
 import 'mdbreact/dist/css/mdb.css';
-import '../public/static/style/main.css'
+
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
