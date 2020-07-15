41d20df5f5876f9fe0092b544dfff8c41f90ee0d
alonecheer <alonecheer@hotmail.com>
2020-07-15 21:05:19 +0700

    end footer

--- pages/footer.js
+++ pages/footer.js
@@ -4,6 +4,20 @@ const Footer = () => {
   return (
     <div>
       <title>Footer</title>
+      <footer>
+        <div className="copyright">
+          <div className="container">
+            <div className="copy-grid">
+            <div className="copytext">
+                <p>Copyright © 2020 Chatchaya - All Rights Reserved</p>
+              </div>
+              <div className="creadit">
+                <p>Meen</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from './Header'
+import Footer from './footer'
 const index = () => {
   return (
     <div>
       <title>Index</title>
       <Header />
+      <Footer />
     </div>
   );
 };
