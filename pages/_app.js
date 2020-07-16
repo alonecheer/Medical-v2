8c643dee94f49f968dce8a0b64f6636cb75ab980
alonecheer <alonecheer@hotmail.com>
2020-07-17 02:09:10 +0700

    mport css

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
+import "../public/static/style/main.css";
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
